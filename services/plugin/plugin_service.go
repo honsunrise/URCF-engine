3cceb0b3cd35714d7faeb2fc508d8577faf9c3c4
zhsyourai <mysoftzhs@gmail.com>
2018-01-29 17:38:31 +0800

    change plugin interface

--- services/plugin/plugin_service.go
+++ services/plugin/plugin_service.go
@@ -1,1 +1,54 @@
 package plugin
+
+import (
+	"github.com/zhsyourai/URCF-engine/models"
+	"github.com/zhsyourai/URCF-engine/repositories"
+)
+
+type InstallFlag int32
+
+const (
+	Reinstall InstallFlag = 1 << iota
+)
+
+type UnInstallFlag int32
+
+const (
+	KeepConfig UnInstallFlag = 1 << iota
+)
+
+type Service interface {
+	GetAll() ([]models.Plugin, error)
+	GetByID(id string) (models.Plugin, error)
+	Uninstall(id string, flag ...UnInstallFlag) error
+	Install(path string, flag ...InstallFlag) (models.Plugin, error)
+	GetInterface(id string) (error)
+}
+
+func NewPluginService(repo repositories.PluginRepository) Service {
+	return &pluginService{repo: repo}
+}
+
+type pluginService struct {
+	repo repositories.PluginRepository
+}
+
+func (s *pluginService) GetAll() ([]models.Plugin, error) {
+	return nil, nil
+}
+
+func (s *pluginService) GetByID(id string) (models.Plugin, error) {
+	return models.Plugin{}, nil
+}
+
+func (s *pluginService) Uninstall(id string, flag ...UnInstallFlag) error {
+	return nil
+}
+
+func (s *pluginService) Install(path string, flag ...InstallFlag) (models.Plugin, error) {
+	return models.Plugin{}, nil
+}
+
+func (s *pluginService) GetInterface(id string) (error) {
+	return nil
+}
