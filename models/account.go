9543cc68c20a350d9b384bd9bce8ca9105024a03
zhsyourai <mysoftzhs@gmail.com>
2018-01-29 16:11:36 +0800

    Add Account models

--- models/account.go
+++ models/account.go
@@ -2,27 +2,11 @@ package models
 
 import "time"
 
-type OAuthType int
-
-const (
-	GOOGLE   OAuthType = iota
-	WECHAT
-	QQ
-	SINA
-	FACEBOOK
-	TWITTER
-)
-
 type Account struct {
-	UID                   string
-	Email                 string
-	CreateDate            time.Time
-	UnionIds              map[OAuthType]string
-	Password              string
-	AccountNonExpired     bool
-	AccountNonLocked      bool
-	CredentialsNonExpired bool
-	Role                  []string
-	Enabled               bool
-	UpdateDate            time.Time
+	ID         string
+	CreateDate time.Time
+	Password   string
+	Role       []string
+	Enabled    bool
+	UpdateDate time.Time
 }
