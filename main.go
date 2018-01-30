2dc8cc4dc4b2bbfb6d78566cf1975a57bb5e30c3
zhsyourai <mysoftzhs@gmail.com>
2018-01-30 17:59:45 +0800

    Add daemon support

--- main.go
+++ main.go
@@ -1,82 +1,146 @@
-package engine
+package main
 
 import (
-	stdContext "context"
-	"github.com/kataras/iris"
-	"github.com/dgrijalva/jwt-go"
-	jwtMiddleware "github.com/iris-contrib/middleware/jwt"
-	prometheusMiddleware "github.com/iris-contrib/middleware/prometheus"
-	"github.com/iris-contrib/middleware/secure"
-	"github.com/iris-contrib/middleware/cors"
-	"github.com/didip/tollbooth"
-	"github.com/iris-contrib/middleware/tollboothic"
-	"time"
-	"github.com/zhsyourai/teddy-backend/uaa/controllers"
+	"os"
+	"syscall"
+	"path"
+	"path/filepath"
+	"github.com/sevlyar/go-daemon"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"os/signal"
 )
 
-func myHandler(ctx iris.Context) {
-	user := ctx.Values().Get("jwt").(*jwt.Token)
+var (
+	app = kingpin.New("urcf-engine", "Universal Remote Config Framework Engine")
 
-	ctx.Writef("This is an authenticated request\n")
-	ctx.Writef("Claim content:\n")
+	serve      = app.Command("serve", "Create URCF daemon.")
+	configFile = serve.Flag("config-file", "Config file location").String()
 
-	ctx.Writef("%s", user.Signature)
-}
+	serveStop = app.Command("kill", "Kill daemon URCF.")
+
+	version        = app.Command("version", "get version")
+	currentVersion = "0.1.0"
+)
 
 func main() {
-	s := secure.New(secure.Options{
-		AllowedHosts:            []string{"ssl.example.com"},                                                                                                                         // AllowedHosts is a list of fully qualified domain names that are allowed. Default is empty list, which allows any and all host names.
-		SSLRedirect:             true,                                                                                                                                                // If SSLRedirect is set to true, then only allow HTTPS requests. Default is false.
-		SSLTemporaryRedirect:    false,                                                                                                                                               // If SSLTemporaryRedirect is true, the a 302 will be used while redirecting. Default is false (301).
-		SSLHost:                 "ssl.example.com",                                                                                                                                   // SSLHost is the host name that is used to redirect HTTP requests to HTTPS. Default is "", which indicates to use the same host.
-		SSLProxyHeaders:         map[string]string{"X-Forwarded-Proto": "https"},                                                                                                     // SSLProxyHeaders is set of header keys with associated values that would indicate a valid HTTPS request. Useful when using Nginx: `map[string]string{"X-Forwarded-Proto": "https"}`. Default is blank map.
-		STSSeconds:              315360000,                                                                                                                                           // STSSeconds is the max-age of the Strict-Transport-Security header. Default is 0, which would NOT include the header.
-		STSIncludeSubdomains:    true,                                                                                                                                                // If STSIncludeSubdomains is set to true, the `includeSubdomains` will be appended to the Strict-Transport-Security header. Default is false.
-		STSPreload:              true,                                                                                                                                                // If STSPreload is set to true, the `preload` flag will be appended to the Strict-Transport-Security header. Default is false.
-		ForceSTSHeader:          false,                                                                                                                                               // STS header is only included when the connection is HTTPS. If you want to force it to always be added, set to true. `IsDevelopment` still overrides this. Default is false.
-		FrameDeny:               true,                                                                                                                                                // If FrameDeny is set to true, adds the X-Frame-Options header with the value of `DENY`. Default is false.
-		CustomFrameOptionsValue: "SAMEORIGIN",                                                                                                                                        // CustomFrameOptionsValue allows the X-Frame-Options header value to be set with a custom value. This overrides the FrameDeny option.
-		ContentTypeNosniff:      true,                                                                                                                                                // If ContentTypeNosniff is true, adds the X-Content-Type-Options header with the value `nosniff`. Default is false.
-		BrowserXSSFilter:        true,                                                                                                                                                // If BrowserXssFilter is true, adds the X-XSS-Protection header with the value `1; mode=block`. Default is false.
-		ContentSecurityPolicy:   "default-src 'self'",                                                                                                                                // ContentSecurityPolicy allows the Content-Security-Policy header value to be set with a custom value. Default is "".
-		PublicKey:               `pin-sha256="base64+primary=="; pin-sha256="base64+backup=="; max-age=5184000; includeSubdomains; report-uri="https://www.example.com/hpkp-report"`, // PublicKey implements HPKP to prevent MITM attacks with forged certificates. Default is "".
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case serveStop.FullCommand():
+		stopServer()
+	case serve.FullCommand():
+		startServer()
+	case version.FullCommand():
+		fmt.Println(currentVersion)
+	}
+}
 
-		IsDevelopment: true, // This will cause the AllowedHosts, SSLRedirect, and STSSeconds/STSIncludeSubdomains options to be ignored during development. When deploying to production, be sure to set this to false.
-	})
+func start() (err error) {
+	return
+}
 
-	app := iris.New()
+func stop() (err error) {
+	return
+}
 
-	limiter := tollbooth.NewLimiter(1, nil)
-	m := prometheusMiddleware.New("serviceName", 300, 1200, 5000)
-	crs := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"}, // allows everything, use that to change the hosts.
-		AllowCredentials: true,
-	})
-	jwtHandler := jwtMiddleware.New(jwtMiddleware.Config{
-		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-			return []byte("My Secret"), nil
-		},
-		// When set, the middleware verifies that tokens are signed with the specific signing algorithm
-		// If the signing method is not constant the ValidationKeyGetter callback can be used to implement additional checks
-		// Important to avoid security issues described here: https://auth0.com/blog/2015/03/31/critical-vulnerabilities-in-json-web-token-libraries/
-		SigningMethod: jwt.SigningMethodRS256,
-	})
+func isDaemonRunning(ctx *daemon.Context) (bool, *os.Process, error) {
+	d, err := ctx.Search()
 
-	app.Use(m.ServeHTTP)
-	app.Use(jwtHandler.Serve)
-	app.Use(s.Serve)
-	app.Use(crs)
-	app.Get("/", tollboothic.LimitHandler(limiter), myHandler)
+	if err != nil {
+		return false, d, err
+	}
 
-	app.Controller("/uaa", new(controllers.AccountController))
+	if err := d.Signal(syscall.Signal(0)); err != nil {
+		return false, d, err
+	}
 
-	iris.RegisterOnInterrupt(func() {
-		timeout := 5 * time.Second
-		ctx, cancel := stdContext.WithTimeout(stdContext.Background(), timeout)
-		defer cancel()
-		// close all hosts
-		app.Shutdown(ctx)
-	})
+	return true, d, nil
+}
 
-	app.Run(iris.Addr(":8080"), iris.WithoutInterruptHandler, iris.WithConfiguration(iris.YAML("./configs/iris.yml")))
+func getCtx() *daemon.Context {
+	if *configFile == "" {
+		folderPath := os.Getenv("HOME")
+		*configFile = folderPath + "/.URCF/config.yml"
+		os.MkdirAll(path.Dir(*configFile), 0755)
+	}
+
+	ctx := &daemon.Context{
+		PidFileName: path.Join(filepath.Dir(*configFile), "main.pid"),
+		PidFilePerm: 0644,
+		LogFileName: path.Join(filepath.Dir(*configFile), "main.log"),
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+	}
+	return ctx
+}
+
+func waitForStartResult() bool {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGUSR1, syscall.SIGUSR2)
+	waitedSignal := <-signalChan
+	if waitedSignal == syscall.SIGUSR1 {
+		return true
+	}
+	return false
+}
+
+func sendSignal(pid int, signal os.Signal) error {
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+	defer p.Release()
+	return p.Signal(signal)
+}
+
+func startServer() {
+	ctx := getCtx()
+	if ok, _, _ := isDaemonRunning(ctx); ok {
+		log.Info("Server daemon is already running.")
+		return
+	}
+
+	d, err := ctx.Reborn()
+	if err != nil {
+		log.Fatalf("Failed to reborn daemon due to %+v.", err)
+	}
+
+	if d != nil {
+		waitForStartResult()
+		log.Info("Server daemon started")
+		return
+	}
+	defer ctx.Release()
+
+	log.Info("Starting server daemon...")
+	err = start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sendSignal(os.Getppid(), syscall.SIGUSR1)
+	log.Info("Server daemon started")
+	sigKill := make(chan os.Signal, 1)
+	signal.Notify(sigKill, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	<-sigKill
+	log.Info("Received signal to stop...")
+	err = stop()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(0)
+}
+
+func stopServer() {
+	log.Info("Stopping server daemon ...")
+	ctx := getCtx()
+	if ok, p, _ := isDaemonRunning(ctx); ok {
+		if err := p.Signal(syscall.Signal(syscall.SIGQUIT)); err != nil {
+			log.Fatalf("Failed to kill server daemon %v", err)
+		}
+	} else {
+		ctx.Release()
+		log.Info("Server Instance is not running.")
+	}
+	log.Info("Server daemon terminated")
 }
