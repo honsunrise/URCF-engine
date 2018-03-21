ca8e6b12541b242bc99dce050a1cb4d17e455258
zhsyourai <mysoftzhs@gmail.com>
2018-03-21 18:32:33 +0800

    Add Semantic Version impl

--- services/plugin/version.go
+++ services/plugin/version.go
@@ -1,9 +1,214 @@
 package plugin
 
+import (
+	"strings"
+	"errors"
+	"strconv"
+)
+
 const CoreProtocolVersion = 1
 
+type DetailCompareResult int32
+
+const (
+	NoDifferent                      = 0
+	MajorLT      DetailCompareResult = 1 << iota
+	MajorGT
+	MinorLT
+	MinorGT
+	PatchLT
+	PatchGT
+	PreReleaseLT
+	PreReleaseGT
+	BuildLT
+	BuildGT
+)
+
+type CompareResult int32
+
+const (
+	Same CompareResult = iota
+	GT
+	LT
+)
+
 type SemanticVersion struct {
-	Major uint16
-	Minor uint16
-	Patch uint16
+	Major      uint32
+	Minor      uint32
+	Patch      uint32
+	PreRelease []string
+	Build      []string
+	valid      bool
+}
+
+func NewSemVerFromString(ver string) (semVer *SemanticVersion, err error) {
+	semVer = &SemanticVersion{
+		Major: 0, Minor: 0, Patch: 0, valid: false,
+	}
+	tmp := strings.SplitN(ver, "+", 2)
+	if len(tmp) == 2 {
+		semVer.Build = strings.Split(tmp[1], ".")
+		ver = tmp[0]
+	} else if len(tmp) != 1 {
+		err = errors.New("semantic version format error")
+		return
+	}
+	tmp = strings.SplitN(ver, "-", 2)
+	if len(tmp) == 2 {
+		semVer.PreRelease = strings.Split(tmp[1], ".")
+		ver = tmp[0]
+	} else if len(tmp) != 1 {
+		err = errors.New("semantic version format error")
+		return
+	}
+	tmp = strings.SplitN(ver, ".", 3)
+	if len(tmp) == 3 {
+		ui64 := uint64(0)
+		ui64, err = strconv.ParseUint(tmp[2], 10, 0)
+		if err != nil {
+			return
+		}
+		semVer.Patch = uint32(ui64)
+
+		ui64, err = strconv.ParseUint(tmp[1], 10, 0)
+		if err != nil {
+			return
+		}
+		semVer.Minor = uint32(ui64)
+
+		ui64, err = strconv.ParseUint(tmp[0], 10, 0)
+		if err != nil {
+			return
+		}
+		semVer.Major = uint32(ui64)
+		semVer.valid = true
+	} else if len(tmp) != 1 {
+		err = errors.New("semantic version format error")
+		return
+	}
+	return
+}
+
+func arrayCompare(this []string, other []string) int {
+	thisLeft := len(this)
+	OtherLeft := len(other)
+	if thisLeft == 0 && OtherLeft > 0 {
+		return 1
+	} else if thisLeft > 0 && OtherLeft == 0 {
+		return -1
+	}
+	i := 0
+	for {
+		if thisLeft == 0 && OtherLeft > 0 {
+			return -1
+		} else if thisLeft > 0 && OtherLeft == 0 {
+			return 1
+		} else if thisLeft == 0 && OtherLeft == 0 {
+			return 0
+		} else {
+			ui64this, err := strconv.ParseUint(this[i], 10, 0)
+			if err == nil {
+				ui64other, err := strconv.ParseUint(other[i], 10, 0)
+				if err == nil {
+					if ui64this < ui64other {
+						return -1
+					} else if ui64this > ui64other {
+						return 1
+					}
+					return 0
+				}
+			}
+
+			result := strings.Compare(this[i], other[i])
+			if result != 0 {
+				return result
+			}
+			thisLeft -= 1
+			OtherLeft -= 1
+			i++
+		}
+	}
+}
+
+func (semVer *SemanticVersion) DetailCompare(other *SemanticVersion) DetailCompareResult {
+	if !other.valid || !semVer.valid {
+		panic(errors.New("invalid SemanticVersion can't compare"))
+	}
+	var result DetailCompareResult = NoDifferent
+
+	if semVer.Major < other.Major {
+		result |= MajorLT
+	} else if semVer.Major > other.Major {
+		result |= MajorGT
+	}
+
+	if semVer.Minor < other.Minor {
+		result |= MinorLT
+	} else if semVer.Minor > other.Minor {
+		result |= MinorGT
+	}
+
+	if semVer.Patch < other.Patch {
+		result |= PatchLT
+	} else if semVer.Patch > other.Patch {
+		result |= PatchGT
+	}
+
+	cr := arrayCompare(semVer.PreRelease, other.PreRelease)
+	if cr < 0 {
+		result |= PreReleaseLT
+	} else if cr > 0 {
+		result |= PreReleaseGT
+	}
+
+	cr = arrayCompare(semVer.Build, other.Build)
+	if cr < 0 {
+		result |= BuildLT
+	} else if cr > 0 {
+		result |= BuildGT
+	}
+
+	return result
+}
+
+func (semVer *SemanticVersion) Compare(other *SemanticVersion) CompareResult {
+	result := semVer.DetailCompare(other)
+	if (result & MajorLT) != 0 {
+		return LT
+	} else if (result & MajorGT) != 0 {
+		return GT
+	} else {
+		if (result & MinorLT) != 0 {
+			return LT
+		} else if (result & MinorGT) != 0 {
+			return GT
+		} else {
+			if (result & PatchLT) != 0 {
+				return LT
+			} else if (result & PatchGT) != 0 {
+				return GT
+			} else {
+				if (result & PreReleaseLT) != 0 {
+					return LT
+				} else if (result & PreReleaseGT) != 0 {
+					return GT
+				} else {
+					return Same
+				}
+			}
+		}
+	}
+}
+
+func (semVer *SemanticVersion) Compatible(other *SemanticVersion) bool {
+	result := semVer.DetailCompare(other)
+	if (result&MajorLT) != 0 || (result&MajorGT) != 0 {
+		return false
+	} else {
+		if (result & MinorGT) != 0 {
+			return false
+		} else {
+			return true
+		}
+	}
 }
---
+++ services/plugin/version_test.go
@@ -0,0 +1,51 @@
+package plugin
+
+import (
+	"testing"
+	"errors"
+)
+
+
+// 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta < 1.0.0-beta.2 < 1.0.0-beta.11111
+// < 1.0.0-beta.k < 1.0.0-rc.1 < 1.0.0
+func TestSemanticVersion_Compare(t *testing.T) {
+	testList := []string {
+		"1.0.0-alpha",
+		"1.0.0-alpha.1",
+		"1.0.0-alpha.beta",
+		"1.0.0-beta",
+		"1.0.0-beta.2",
+		"1.0.0-beta.11111",
+		"1.0.0-beta.k",
+		"1.0.0-rc.1",
+		"1.0.0",
+	}
+
+	for i := 0; i < len(testList); i++ {
+		for j := 0; j < len(testList); j++ {
+			v1, err := NewSemVerFromString(testList[i])
+			if err != nil {
+				t.Fatal(err)
+			}
+			v2, err := NewSemVerFromString(testList[j])
+			if err != nil {
+				t.Fatal(err)
+			}
+			if (i == j) {
+				if v1.Compare(v2) != Same {
+					t.Fatal(errors.New("Version compare not correct"))
+				}
+			} else if i < j {
+				if v1.Compare(v2) != LT {
+					t.Fatal(errors.New("Version compare not correct"))
+				}
+			} else {
+				if v1.Compare(v2) != GT {
+					t.Fatal(errors.New("Version compare not correct"))
+				}
+			}
+		}
+	}
+
+
+}
