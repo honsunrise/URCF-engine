8ccc8d8840194bcce31c65ade3cabe17a115c10b
zhsyourai <mysoftzhs@gmail.com>
2018-04-11 19:20:09 +0800

    Add log repository

---
+++ models/log.go
@@ -0,0 +1,65 @@
+package models
+
+import (
+	"time"
+	"strings"
+	"fmt"
+)
+
+type Level uint32
+
+func (level Level) String() string {
+	switch level {
+	case DebugLevel:
+		return "debug"
+	case InfoLevel:
+		return "info"
+	case WarnLevel:
+		return "warning"
+	case ErrorLevel:
+		return "error"
+	case FatalLevel:
+		return "fatal"
+	case PanicLevel:
+		return "panic"
+	}
+
+	return "unknown"
+}
+
+func ParseLevel(lvl string) (Level, error) {
+	switch strings.ToLower(lvl) {
+	case "panic":
+		return PanicLevel, nil
+	case "fatal":
+		return FatalLevel, nil
+	case "err", "error":
+		return ErrorLevel, nil
+	case "warn", "warning":
+		return WarnLevel, nil
+	case "info":
+		return InfoLevel, nil
+	case "debug":
+		return DebugLevel, nil
+	}
+
+	var l Level
+	return l, fmt.Errorf("not a valid Level: %q", lvl)
+}
+
+const (
+	PanicLevel Level = iota
+	FatalLevel
+	ErrorLevel
+	WarnLevel
+	InfoLevel
+	DebugLevel
+)
+
+type Log struct {
+	ID         uint64
+	CreateDate time.Time
+	Message    string
+	Name       string
+	Level      Level
+}
