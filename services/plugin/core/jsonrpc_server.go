bfa38d65db230fd568d70da4c021655ebf26abfe
Hobson <mysoftzhs@gmail.com>
2018-07-09 15:57:58 +0800

    Fix plugin server logic

--- services/plugin/core/jsonrpc_server.go
+++ services/plugin/core/jsonrpc_server.go
@@ -9,6 +9,7 @@ import (
 	"golang.org/x/net/websocket"
 	"net"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -17,6 +18,7 @@ var ErrCannotConnectTwice = errors.New("can't connect twice")
 type warpPlugin struct {
 	name      string
 	rpcClient *jsonrpc2.Client
+	exit      chan struct{}
 }
 
 type commandParam struct {
@@ -70,45 +72,61 @@ func (JsonRPCFactory) New(rpi RegisterPluginInterface) (ServerInterface, error) 
 type JsonRPCServer struct {
 	httpServer *http.Server
 	rpi        RegisterPluginInterface
+	connMap    sync.Map
 }
 
-func (s *JsonRPCServer) serverPlugin(rpcClient *jsonrpc2.Client) (string, error) {
+func (s *JsonRPCServer) serverPlugin(ws *websocket.Conn) error {
+	rpcClient := jsonrpc2.NewClient(ws)
 	info := &PluginReportInfo{}
-	err := rpcClient.Call("Plugin.GetPluginInfo", nil, info)
+	var err error = nil
+	err = rpcClient.Call("Plugin.GetPluginInfo", nil, info)
 	if err != nil {
-		return "", err
+		return err
 	}
-	if s.rpi.IsRegister(info.Name) {
-		return info.Name, ErrCannotConnectTwice
+	result, loaded := s.connMap.Load(info.Name)
+	if loaded {
+		lastWp := result.(*warpPlugin)
+		var pong string
+		err := lastWp.rpcClient.Call("Plugin.Ping", nil, &pong)
+		if err != nil {
+			s.rpi.UnRegister(info.Name)
+			s.connMap.Delete(info.Name)
+			close(lastWp.exit)
+		} else {
+			return ErrCannotConnectTwice
+		}
 	}
 	wp := &warpPlugin{
 		name:      info.Name,
 		rpcClient: rpcClient,
+		exit:      make(chan struct{}),
 	}
 	err = s.rpi.Register(info.Name, wp)
 	if err != nil {
-		return info.Name, err
+		return err
 	}
 
+	s.connMap.Store(info.Name, wp)
+
 	for true {
-		<-time.After(10 * time.Second)
-		var pong string
-		err := rpcClient.Call("Plugin.Ping", nil, &pong)
-		if err != nil {
-			return info.Name, err
+		select {
+		case <-time.After(10 * time.Second):
+			var pong string
+			err := rpcClient.Call("Plugin.Ping", nil, &pong)
+			if err != nil {
+				return err
+			}
+		case <-wp.exit:
+			return nil
 		}
 	}
-	return info.Name, nil
+	return nil
 }
 
 func (s *JsonRPCServer) Serve(lis net.Listener, TLS *tls.Config) error {
 	handler := websocket.Server{Handler: func(ws *websocket.Conn) {
-		rpcClient := jsonrpc2.NewClient(ws)
-		pluginName, err := s.serverPlugin(rpcClient)
+		err := s.serverPlugin(ws)
 		if err != nil {
-			if s.rpi.IsRegister(pluginName) {
-				s.rpi.UnRegister(pluginName)
-			}
 			log.Error(err)
 		}
 	}}
